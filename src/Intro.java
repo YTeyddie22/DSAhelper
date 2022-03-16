ae9a9981703f72ffaadd0d86040ce326bf814821
Teyddie22 <teyddie@gmail.com>
2022-03-16 11:02:53 +0300

    Introduction

---
+++ src/Intro.java
@@ -0,0 +1,6 @@
+public class Intro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!! These algorithms will aid you in solving problems you might face");
+    }
+}
