d141e7a7624e467377097ce6cf2f27ef3f847fc8
Teddy Yongo <teyddie@gmail.com>
2022-05-06 13:08:20 +0300

    Palindrome checker

--- src/strings/Builder.java
+++ src/strings/Builder.java
@@ -1,4 +1,25 @@
 package strings;
 
+import java.util.Locale;
+
 public class Builder {
+
+    public static void main(String[] args) {
+        String ans = "mUm";
+        System.out.println(isPalindrome(ans));
+    }
+
+    static boolean isPalindrome(String str){
+        str = str.toLowerCase();
+
+        for (int i = 0; i <= str.length()/2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+
+            if(start !=end) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
