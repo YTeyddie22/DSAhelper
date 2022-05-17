282695d6eba213bb41ebc205b6ef8aff537b211c
Teddy Yongo <teyddie@gmail.com>
2022-05-17 11:04:10 +0300

    third pattern star

---
+++ src/strings/patterns/StringPattern3.java
@@ -0,0 +1,23 @@
+package strings.patterns;
+
+public class StringPattern3 {
+
+    public static void main(String[] args) {
+
+        pattern3(6);
+    }
+
+    static void pattern3(int num){
+
+
+        for (int row = 1; row <=num ; row++) {
+
+            //For every row (subtract the parameter number from the number of row at i then add one
+
+            for (int col = 1; col <= num-row+1 ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
