9f1a718607cb04a37d0da665f142fc6ad8fc58ab
Teddy Yongo <teddy@aeeiee.com>
2023-09-30 12:09:15 +0300

    Ceiling and Floor

---
+++ src/Ceiling.java
@@ -0,0 +1,34 @@
+public class Ceiling {
+
+    public static void main(String[] args) {
+
+        int[] arr = {11,22,44,55,66,77,88};
+        int target = 45;
+        int ans = ceiling(arr, target);
+
+        System.out.println(ans);
+    }
+    static int ceiling(int[] arr,int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        return binarySearch(arr, target,start, end);
+    }
+
+    // Returns the smallest number in a range of numbers
+    // After the target
+    static int binarySearch(int[] arr,int target,int start, int end){
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(target < arr[mid]) {
+                end = mid - 1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return start;
+    }
+}
---
+++ src/Floor.java
@@ -0,0 +1,35 @@
+public class Floor {
+
+    public static void main(String[] args) {
+
+        int[] arr = {11,22,44,55,66,77,88};
+        int target = 45;
+        int ans = floor(arr, target);
+
+        System.out.println(ans);
+    }
+
+    static int floor(int[] arr,int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        return binarySearch(arr, target,start, end);
+    }
+
+    // Returns the greatest number in a range of numbers
+    // Before the target
+    static int binarySearch(int[] arr,int target,int start, int end){
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(target < arr[mid]) {
+                end = mid - 1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return end;
+    }
+}
